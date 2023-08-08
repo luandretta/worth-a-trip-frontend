481563c454213eb3381217596044fbda0d71205d
Lucimeri Andretta <luandretta@hotmail.com>
2023-08-08 13:54:12 +0000

    Feat: create an individual Route component for each url
    Add NavLink components to navigation and set their to prop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
-        <h1>Home</h1>
-        <h1>Sing in</h1>
+        <Switch>
+          <Route  exact path='/' render={() => <h1>Home page</h1>} />
+          <Route  exact path='/signin' render={() => <h1>Sing In</h1>} />
+          <Route  exact path='/signup' render={() => <h1>Sing Up</h1>} />
+        </Switch>
       </Container>
     </div>
   );
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,48 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import logo from '../assets/logo.png';
-import styles from '../styles/NavBar.module.css';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <Navbar className={styles.NavBar} expand='md' fixed='top'>
-        <Container>
-            <Navbar.Brand>
-                <img src={logo} alt='logo' height='80' />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls='basic-navbar-nav' />
-            <Navbar.Collapse id='basic-navbar-nav'>
-                <Nav className='ml-auto text-left'>
-                    <Nav.Link>
-                        <i className='fas fa-home'></i>Home
-                    </Nav.Link>
-                    <Nav.Link>
-                        <i className='fas fa-sign-in-alt'></i>Sign in
-                    </Nav.Link>
-                    <Nav.Link>
-                        <i className='fas fa-user-plus'></i>Sign up
-                    </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Container>
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="60" />
+          </Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 };
