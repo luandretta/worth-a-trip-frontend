42db904d6681079801e27af7815815f41fdce8ca
Lucimeri Andretta <luandretta@hotmail.com>
2023-08-08 12:05:12 +0000

    Feat: create NavBar component
    Create assets and logo to NavBar component and favicon to index.html

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <NavBar />
     </div>
   );
 }
